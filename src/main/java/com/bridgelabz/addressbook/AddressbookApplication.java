28162db87a57d88dd605e3b2177fe9eefd5d1795
mehulbhange <bhangemehul@gmail.com>
2022-10-16 19:30:15 +0530

    ADDED:[Mehul] determine the logging levels based on app running in dev, staging or prod

--- src/main/java/com/bridgelabz/addressbook/AddressbookApplication.java
+++ src/main/java/com/bridgelabz/addressbook/AddressbookApplication.java
@@ -3,14 +3,17 @@ package com.bridgelabz.addressbook;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 @Slf4j
 public class AddressbookApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(AddressbookApplication.class, args);
+        ApplicationContext context = SpringApplication.run(AddressbookApplication.class, args);
         log.info("Address book application started");
+        log.info("Address book application started in "+context.getEnvironment().getProperty("environment")+" Environment");
+
     }
 
 }
