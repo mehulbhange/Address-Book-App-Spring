d86306b79a94b399d78607d17de44f90b22a56b2
mehulbhange <bhangemehul@gmail.com>
2022-10-29 19:22:49 +0530

    ADDED:[Mehul] model mapper to convert dto to entity and vice versa

--- src/main/java/com/bridgelabz/addressbook/service/AddressBookServiceImpl.java
+++ src/main/java/com/bridgelabz/addressbook/service/AddressBookServiceImpl.java
@@ -4,6 +4,7 @@ import com.bridgelabz.addressbook.dto.AddressBookDTO;
 import com.bridgelabz.addressbook.entity.AddressBook;
 import com.bridgelabz.addressbook.exceptions.AddressBookException;
 import com.bridgelabz.addressbook.repository.AddressBookRepository;
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -17,6 +18,9 @@ public class AddressBookServiceImpl implements IAddressBookService{
     //List<AddressBook> addressBookList = new ArrayList<>();
     @Autowired
     private AddressBookRepository addressBookRepo;
+
+    @Autowired
+    private ModelMapper modelMapper;
 
     /*This method is used to get all the records from db table*/
     @Override
@@ -34,8 +38,7 @@ public class AddressBookServiceImpl implements IAddressBookService{
     * */
     @Override
     public ResponseEntity<AddressBook> createAddressBookData(AddressBookDTO addressBookDTO) {
-        AddressBook addressBook = new AddressBook(addressBookDTO);
-        addressBookRepo.save(addressBook);
+        AddressBook addressBook = addressBookRepo.save(modelMapper.map(addressBookDTO, AddressBook.class));
         return new ResponseEntity<>(addressBook, HttpStatus.OK);
     }
     /*This method takes two arguments id and AddressBookDTO
@@ -45,13 +48,8 @@ public class AddressBookServiceImpl implements IAddressBookService{
     public ResponseEntity<AddressBook> updateAddressBookData(long personId, AddressBookDTO addressBookDTO) {
         AddressBook addressBook = addressBookRepo.findById(personId).orElseThrow(() -> new AddressBookException("Contact not found with id : "+personId));
         if (addressBook != null){
-            addressBook.setFirstName(addressBookDTO.getFirstName());
-            addressBook.setLastName(addressBookDTO.getLastName());
-            addressBook.setContactNo(addressBookDTO.getContactNo());
-            addressBook.setEmail(addressBook.getEmail());
-            addressBook.setCity(addressBookDTO.getCity());
-            addressBook.setState(addressBookDTO.getState());
-            addressBook.setZip(addressBookDTO.getZip());
+            addressBook = modelMapper.map(addressBookDTO, AddressBook.class);
+            addressBook.setId(personId);
             addressBookRepo.save(addressBook);
         }
         return new ResponseEntity<>(addressBook, HttpStatus.OK);
